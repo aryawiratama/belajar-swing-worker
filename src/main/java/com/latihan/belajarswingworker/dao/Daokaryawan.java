51a02bb18874caf0552f606b5b34fd67bcafe8e7
aryawiratama <stop.drunk@gmail.com>
2014-12-21 23:04:35 +0700

    add dao karyawan

---
+++ src/main/java/com/latihan/belajarswingworker/dao/Daokaryawan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.latihan.belajarswingworker.dao;
+
+import com.latihan.belajarswingworker.domain.Karyawan;
+import java.util.List;
+
+/**
+ *
+ * @author Artha
+ */
+public interface Daokaryawan {
+    public List<Karyawan> getKaryawans();
+}
